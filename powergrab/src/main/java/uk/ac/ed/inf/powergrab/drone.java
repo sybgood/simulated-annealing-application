f56f47c586e3014577f8333d787de335cc893095
sybgood <sybtxd1@yahoo.com>
2019-10-18 19:53:35 +0100

    Add sort methods, which can sort the coordinate list by  distance to nearest station.

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/drone.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/drone.java
@@ -1,7 +1,9 @@
 package uk.ac.ed.inf.powergrab;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Random;
 
 public abstract class drone {
@@ -10,7 +12,7 @@ public abstract class drone {
     protected int steps = 250;
     protected Position curr;
     protected Map map;
-    protected Random rnd;
+    protected final Random rnd;
     protected String nearStation="";
 
     public drone(Double latitude,Double longitude,int seed,Map map) {
@@ -40,29 +42,43 @@ public abstract class drone {
         if (power>1.25&&steps>0) return true;
         return false;
     }
-    
+
     public HashMap<Direction,String> haveStation(Position p) { 
         HashMap<Direction,String> k = new HashMap<Direction,String>();
         // wait for map.getCoorList().sort()
+        int i;
         for (Direction d : Direction.values()) {
+            i = 0;
+            Position coor;
             Position nextp = curr.nextPosition(d);
-            if(nextp.inPlayArea()) {
-                for (Position coor: map.getCoorList()) {
-                    if(isNear(nextp,coor)) {
-                        k.put(d,map.CoordinateId.get(coor));
-                    }
-                }
+            ArrayList<Position> CoorList = map.getCoorList();
+            Collections.sort(CoorList,new DistanceComp(nextp));
+            while(true) {
+                coor = CoorList.get(i);
+                if(isNear(nextp,coor)) k.put(d,map.CoordinateId.get(coor));
+                else break;
+                i++;
             }
+//            if(nextp.inPlayArea()) {
+//                for (Position coor: map.getCoorList()) {
+//                    if(isNear(nextp,coor)) {
+//                        k.put(d,map.CoordinateId.get(coor));
+//                    }
+//                }
+//            }
         }
         return k;
     }
-    
-    public static Boolean isNear(Position p1, Position p2) {
+    public static Double CalDistance(Position p1, Position p2) {
         Double x1 = p1.latitude;
         Double x2 = p2.latitude;
         Double y1 = p1.longitude;
         Double y2 = p2.longitude;
-        return (Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2))<=0.00025);
+        return (Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2)));
+    }
+    
+    public  Boolean isNear(Position p1, Position p2) {
+        return (CalDistance(p1,p2)<=0.00025);
     }
     
     public void meetChargeStation(String ID) {
@@ -96,5 +112,21 @@ public abstract class drone {
             map.IDcoins.put(ID,Station_coin);
             map.IDpower.put(ID,Station_power);
         }
+    }
+    class DistanceComp implements Comparator<Position>{
+        private final Position TargetP;
+        public DistanceComp(Position p) {
+            TargetP = p;
+        }
+        @Override
+        public int compare(Position p1, Position p2) {
+            Double a = drone.CalDistance(p1,TargetP);
+            Double b = drone.CalDistance(p2, TargetP);
+            return (a>b) ? 1 : -1 ;
+        }
+
+        
+
+        
     }
 }
