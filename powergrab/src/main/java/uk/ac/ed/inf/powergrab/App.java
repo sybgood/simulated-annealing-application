f31eff2ed61818cac6c4cadc6f5c3fb828ef49d2
sybgood <sybtxd1@yahoo.com>
2019-10-28 22:56:56 +0000

    Small change. Can be ignore.

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -23,31 +23,47 @@ public class App
         String difficulty = args[6];
         
         Map map = new Map(year,month,day);
+        Double totalGold=0.0;
+        for(String k:map.IDcoins.keySet()) {
+            Double g = map.IDcoins.get(k);
+            if(g>0) totalGold+=g;
+        }
+        Annealing a = new Annealing(map,40,500,200.0f, 0.995f,5);
+        a.solve();
+
        // for (Position p :map.getCoorList()) {
        //     System.out.print(p.latitude);
        // }
+        long ab =System.currentTimeMillis();
         Statelessdrone d = new Statelessdrone(latitude,longitude,RNGseed,map);
-        d.statelessMove();
         try {
-            long ab =System.currentTimeMillis();
-            FileOutputStream bos = new FileOutputStream("output.txt");
-            BufferedOutputStream buff = new BufferedOutputStream(bos);
-            String trace = d.statelessMove();
-            buff.write(trace.getBytes());
-            buff.flush();
-            buff.close();
-            map = d.getMap();
+//            FileOutputStream bos = new FileOutputStream("output.txt");
+//            BufferedOutputStream buff = new BufferedOutputStream(bos);
+//            String trace = d.statelessMove();
+//            buff.write(trace.getBytes());
+//            buff.flush();
+//            buff.close();
+//            map = d.getMap();
+//            String json = map.outputJson();
+//            FileOutputStream bos2 = new FileOutputStream("2018.geojson");
+//            buff =new BufferedOutputStream(bos2);
+//            buff.write(json.getBytes());
+//            buff.flush();
+//            long aa = System.currentTimeMillis();
+//            System.out.print(aa-ab);
+            FileOutputStream bos3 = new FileOutputStream("test.geojson");
+            BufferedOutputStream buff = new BufferedOutputStream(bos3);
+            ArrayList<Position> path=a.givePath();
+            for(Position sp:path) {
+                map.addTrace(sp);
+            }
             String json = map.outputJson();
-            FileOutputStream bos2 = new FileOutputStream("2018.geojson");
-            buff =new BufferedOutputStream(bos2);
             buff.write(json.getBytes());
             buff.flush();
-            long aa = System.currentTimeMillis();
-            System.out.print(aa-ab);
-        } catch (FileNotFoundException e) {
+         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        } catch (IOException e) {
+            } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();}
     }
--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Statefuldrone.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Statefuldrone.java
@@ -1,11 +1,20 @@
 package uk.ac.ed.inf.powergrab;
-import java.util.Random;
+
+import java.util.ArrayList;
 
 public class Statefuldrone extends drone {
-
+    private Double totalGold;
+    private ArrayList<Position> stationOrder =new ArrayList<Position>();
     public Statefuldrone(Double latitude, Double longitude,int seed,Map map) {
         super(latitude, longitude, seed, map);
         // TODO Auto-generated constructor stub
     }
-
+    private void transformMap() {
+        Double g;
+        for(String k:map.IDcoins.keySet()) {
+            g=map.IDcoins.get(k);
+            if(g>=0) totalGold+=g;
+        }
+    }
 }
+
