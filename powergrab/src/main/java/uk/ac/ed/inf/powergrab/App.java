161e1e5226dad156d057f144d084427b154fce4f
sybgood <sybtxd1@yahoo.com>
2019-11-11 18:48:52 +0000

    Remake the app class, divided it into some piece of functions

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -10,8 +10,40 @@ import java.util.Random;
 
 public class App 
 {
-   
-    
+    private static void play(Double latitude, Double longitude,int seed,Map map,String difficuity,String date) {
+        if(difficuity.equals("stateless")) {
+            Statelessdrone d= new  Statelessdrone(latitude, longitude, seed, map);
+            String contents = d.statelessMove();
+            String title = difficuity+"-"+date+".txt";
+            writeInComputer(contents,title);
+            contents = d.map.outputJson();
+            title = difficuity+"-"+date+".geojson";
+            writeInComputer(contents,title);
+        }
+        else if(difficuity.equals("stateful")) {
+            Statefuldrone sd = new Statefuldrone(latitude,longitude,seed,map);
+            sd.statefulMove();
+            String contents = sd.returnTrace();
+            String title = difficuity+"-"+date+".txt";
+            writeInComputer(contents,title);
+            contents = sd.map.outputJson();
+            title = difficuity+"-"+date+".geojson";
+            writeInComputer(contents,title);
+        }
+    }
+    private static void writeInComputer(String contents,String title) {
+            FileOutputStream bos;
+            try {
+                bos = new FileOutputStream(title);
+                BufferedOutputStream buff = new BufferedOutputStream(bos);
+                buff.write(contents.getBytes());
+                buff.flush();
+                buff.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+    }
     public static void main( String[] args )
     {
         String day = args[0];
@@ -20,62 +52,12 @@ public class App
         Double latitude = Double.parseDouble(args[3]);
         Double longitude = Double.parseDouble(args[4]);
         Integer RNGseed = Integer.parseInt(args[5]);
-        String difficulty = args[6];
-        
+        String difficuity = args[6];        
         Map map = new Map(year,month,day);
-        Double totalGold = 0.0;
-        HashMap<String,Double> ss =map.IDcoins;
-        for(Double g : ss.values()) {
-            if(g>=0) totalGold+=g;
-        }
-        long ab =System.currentTimeMillis();
-        Position start = new Position(latitude,longitude);
-        Random rnd = new Random(RNGseed);
-//        Annealing a = new Annealing(map,2000,400,200.0f,0.995f,rnd,start);
-//        a.solve();
-        Statefuldrone sd = new Statefuldrone(latitude,longitude,RNGseed,map);
-
-        sd.statefulMove();
-
-       // for (Position p :map.getCoorList()) {
-       //     System.out.print(p.latitude);
-       // }
-        Statelessdrone d = new Statelessdrone(latitude,longitude,RNGseed,map);
-        try {
-            FileOutputStream bos = new FileOutputStream("output.txt");
-            BufferedOutputStream buff = new BufferedOutputStream(bos);
-            String trace = sd.toString();
-            buff.write(trace.getBytes());
-            buff.flush();
-            buff.close();
-            map = sd.getMap();
-            String json = map.outputJson();
-            FileOutputStream bos2 = new FileOutputStream("2018.geojson");
-            buff =new BufferedOutputStream(bos2);
-            buff.write(json.getBytes());
-            buff.flush();
-            long aa = System.currentTimeMillis();
-            
-            System.out.println("\n"+sd.coin/totalGold);
-            System.out.print(aa-ab);
-//            ArrayList<Position> path=a.givePath();
-//            for(Position sp:path) {
-//                map.addTrace(sp);
-//            }
-
-         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();}
+        String date = day+"-"+month+"-"+year; 
+        long ab =System.currentTimeMillis(); 
+        play(latitude,longitude,RNGseed,map,difficuity,date);
+        long aa = System.currentTimeMillis();
+        System.out.print(aa-ab);
     }
-        
-//        Position p1 = new Position(55.943372541675856,-3.185032961811619);
-//        Position p2 = new Position(55.9435,-3.1850);
-//        Position p3 = new Position(55.9436,-3.1847);
-//        Double d1= drone.CalDistance(p1, p2);
-//        Double d2 = drone.CalDistance(p1, p3);
-
-
 }
